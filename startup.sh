4706f9e9edc2c0b22a52596338a4f478110c2389
MPLeung <mpltest2020@gmail.com>
2020-04-14 12:15:50 +0800

    update

--- startup.sh
+++ startup.sh
@@ -1,5 +1,3 @@
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo docker-compose -up
+sudo apt install docker.io -y && sudo systemctl start docker && sudo systemctl enable docker
+sudo apt install docker-compose && sudo docker-compose -up
